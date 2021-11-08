5441230c0cf693ea6f9d3632feb11f768a11d95e
Psycho-Patch <akshaybillore92@gmail.com>
2021-11-08 19:48:17 +0530

    Updates

---
+++ precache-manifest.a62b33272772e0b74b37cd90c16844a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc76b32aee6cb00d94494ab7fdbf7610",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "203962b91c0506f58836",
+    "url": "/TextUtils/static/css/main.db6084ef.chunk.css"
+  },
+  {
+    "revision": "5da4d59910867ec6fb96",
+    "url": "/TextUtils/static/js/2.cd8ee3dc.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/TextUtils/static/js/2.cd8ee3dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9de38e9a26a8fd3fcd",
+    "url": "/TextUtils/static/js/3.4e97f0bf.chunk.js"
+  },
+  {
+    "revision": "203962b91c0506f58836",
+    "url": "/TextUtils/static/js/main.f65282d8.chunk.js"
+  },
+  {
+    "revision": "6f187c1b23c67b9a7c59",
+    "url": "/TextUtils/static/js/runtime-main.840cdbdf.js"
+  }
+]);
